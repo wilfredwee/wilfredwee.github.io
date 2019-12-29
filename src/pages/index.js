871a4d0e6007542501f5b265a44f31e03dba1246
Wilfred Wee <weezhizhiang@hotmail.com>
2019-12-29 00:28:50 -0800

    Reintroduce JS.
    I guess JS can be a good thing. Gatsby plugins have better noscript
    support than I expected. I just need to test with JS disabled.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -11,6 +12,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
