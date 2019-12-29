601552b19098c4ccfb4e983423ac4f858b84b031
Wilfred Wee <weezhizhiang@hotmail.com>
2019-12-28 23:42:18 -0800

    Disable JS

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -30,5 +28,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    'gatsby-plugin-no-javascript',
   ],
 }
